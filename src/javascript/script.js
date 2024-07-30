0e8db218698c9a20ca92ef4641b064b7eacf9006
JGabrielBoss <112196857+JGabrielBoss@users.noreply.github.com>
2024-07-30 04:28:07 -0300

    Dando vida
    Criei a home da landinpage

---
+++ src/javascript/script.js
@@ -0,0 +1,7 @@
+//JQuery
+$(document).ready(function () {
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+});
